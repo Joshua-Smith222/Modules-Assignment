da2265a82768998a7b042d1f24e4e11f71cc2767
Joshua D Smith <2smithjosh@gmail.com>
2025-02-04 13:34:44 -0800

    second commit 2nd task of modules assignment.

---
+++ main2.py
@@ -0,0 +1,6 @@
+import text_utils as tu
+input_string = "taco tuesday"
+reversed_string = tu.reverse_string(input_string)
+print(f"Reversed String: {reversed_string}")
+capitalized_string = tu.capitalize_string(input_string)
+print(f"Capitalized String: {capitalized_string}")
