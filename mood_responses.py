7d6100af6267cf18f798fbd526886143dfa19080
Joshua D Smith <2smithjosh@gmail.com>
2025-02-04 13:21:56 -0800

    first commit mood responses module.

---
+++ mood_responses.py
@@ -0,0 +1,11 @@
+def mood_responses(mood):
+    if mood == "happy":
+        print("Such a beautiful day!")
+    elif mood == "sad":
+        print("Brighter horizons lay ahead!")
+    elif mood == "mad":
+        print("As unsettled as waves in a storm!")
+    elif mood == "scared":
+        print("Courage comes from admitting and confronting our fears!")
+    else:
+        print("Please input a proper mood: happy, sad, mad or scared.")
